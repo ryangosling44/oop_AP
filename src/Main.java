7a0d6a8b20dd4afbef972a7a97a322c7dd20a820
Artsiom <06piskunartem60@gmail.com>
2025-03-10 16:24:04 +0300

    lab2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Point P = new Point();
+        P.x = 3.0;
+        P.y = 5.0;
+        System.out.println(P.toSvg());
+    }
+}
